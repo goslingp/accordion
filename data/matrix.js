f192a67892590245265993e607f1d35ad7edd04e
goslingp <peter.gosling@dkit.ie>
2025-01-28 08:37:09 +0000

    Add files via upload

---
+++ data/matrix.js
@@ -0,0 +1,28 @@
+export const matrix = {
+    courses : [
+        {term: 1,
+            code: "COMM 1111",
+            title: "Communications 1",
+            credits: 3.0,
+            description: "Description 1"
+        },
+        {term: 2,
+            code: "COMM 2222",
+            title: "Communications 2",
+            credits: 3.0,
+            description: "Description 2"
+        },
+        {term: 3,
+            code: "COMM 3333",
+            title: "Communications 3",
+            credits: 3.0,
+            description: "Description 3"
+        },
+        {term: 4,
+            code: "COMM 4444",
+            title: "Communications 4",
+            credits: 3.0,
+            description: "Description 4"
+        },
+    ]
+}
